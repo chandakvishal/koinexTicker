c721b9f06155aaa58752a4f140ebc0d5868f807c
Vishal Chandak <vishalchandak0212@gmail.com>
2018-01-22 23:54:20 +0530

    Add Daily Min-Max Price Section

--- src/components/CurrencyStats.js
+++ src/components/CurrencyStats.js
@@ -13,41 +13,44 @@ const CurrencyStats = (props) => {
 const {
     currencySymbolStyle,
     currencyNameStyle,
-    symbolTextStyle,
+    currencySymbolTextStyle,
+    currenctNameTextStyle,
     currencyPriceTextStyle,
-    currencyPriceViewStyle
+    currencyPriceViewStyle,
+    currencyDetailStyleKey,
+    currencyDetailStyleLeft,
+    currencyDetailStyleRight
 } = styles;
 
 return (
         <Card>
             <CardSection>
-                <View style={currencySymbolStyle}>
-                    <Text style={symbolTextStyle}>
-                        {props.currency.symbol}
+                <View style={currencyNameStyle}>
+                    <Text style={currenctNameTextStyle}>
+                        {GetCurrencyName(props.currency.symbol)}
                     </Text>
                 </View>
-                <View style={currencyNameStyle}>
-                    <Text>{GetCurrencyName(props.currency.symbol)}</Text>
+                <View style={currencySymbolStyle}>
+                    <Text style={currencySymbolTextStyle}>
+                        {props.currency.symbol}
+                    </Text>
                 </View>
             </CardSection>
             <CardSection>
                 <View style={currencyPriceViewStyle}>
                     <Text style={currencyPriceTextStyle}>
-                        {props.currency.price}
+                        ₹{props.currency.price}
                     </Text>
                 </View>
             </CardSection>
-            // <CardSection>
-            //     <View style={currencyPriceViewStyle}>
-            //         <Text>{props.currency.stats.min_24hrs}
-            //         </Text>
-            //     </View>
-            //     <View style={currencyPriceViewStyle}>
-            //         <Text style={currencyPriceTextStyle}>
-            //             {props.currency.price}
-            //         </Text>
-            //     </View>
-            // </CardSection>
+            <CardSection>
+                <View style={currencyDetailStyleLeft}>
+                    <Text style={currencyDetailStyleKey}>Min(24 Hrs): </Text><Text>₹{props.currency.stats.min_24hrs}</Text>
+                </View>
+                <View style={currencyDetailStyleRight}>
+                    <Text style={currencyDetailStyleKey}>Max(24 Hrs): </Text><Text>₹{props.currency.stats.max_24hrs}</Text>
+                </View>
+            </CardSection>
         </Card>
     );
 };
@@ -56,16 +59,21 @@ const styles = {
     currencySymbolStyle: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start'
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end'
     },
     currencyNameStyle: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end'
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
     },
-    symbolTextStyle: {
+    currencySymbolTextStyle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        fontFamily: 'Cochin'
+    },
+    currenctNameTextStyle: {
         fontSize: 20,
         fontWeight: 'bold',
         // fontFamily: 'Cochin'
@@ -79,7 +87,23 @@ const styles = {
     currencyPriceTextStyle: {
         fontSize: 20,
         fontWeight: 'bold',
-    }
+    },
+    currencyDetailStyleKey: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'flex-end',
+        fontWeight: 'bold',
+    },
+    currencyDetailStyleLeft: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'flex-start'
+    },
+    currencyDetailStyleRight: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'flex-end'
+    },
 };
 
 export default CurrencyStats;
